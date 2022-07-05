41644a76f4c6f74e86e5d546f70e6dbd1e1ef750
oshurek_m <max.ashurek@gmil.com>
2022-07-05 15:25:11 +0300

    тестовая модель работает делаем marge

--- model.py
+++ model.py
@@ -9,14 +9,30 @@ def save_excel(path, data_frame, columns=True):
 
 
 # Подготовка исходных данных для расчетов
+# Загрузка исходных данных
 df = pd.read_excel('entry.xlsx', sheet_name='model')
 df_2 = df.dropna(subset='наименование', axis=0)
-df_2.info()
+# Загрузка ограничений
 df_rest = pd.read_excel('restrictions.xlsx')
 # величина смены в часах
-b = 8 * 0.8
+b = df_rest.loc[0, 'delta_smen_hours']
 # кол-во смен в месяце
-c = 6
+c = df_rest.loc[0, 'count_smen']
+# Лимита по часам и кол-ву оборудования
+t_cn = df_rest.loc[0, 'limit_time_cn']
+t_dart = df_rest.loc[0, 'limit_time_dart']
+t_tour = df_rest.loc[0, 'limit_time_tour']
+t_cn4axe = df_rest.loc[0, 'limit_time_cn_4axe']
+'''
+количество центров 18шт
+количество дартов 16шт
+количество токарных 9 шт
+'''
+count_cn = df_rest.loc[0, 'limit_count_cn']
+count_dart = df_rest.loc[0, 'limit_count_dart']
+count_tour = df_rest.loc[0, 'limit_count_tour']
+count_cn4axe = df_rest.loc[0, 'limit_count_cn_4axe']
+
 # Создание модели
 model = LpProblem('prod', LpMaximize)
 # Генерация словарей с значениями переменных
@@ -26,8 +42,25 @@ cn = dict(zip(names_det, df_2['cn']))
 cn_4 = dict(zip(names_det, df_2['4_ось']))
 dart = dict(zip(names_det, df_2['dart']))
 tour = dict(zip(names_det, df_2['tour']))
-
+# Бинарная переменная, по которой будем определять изготоавливать деталь или нет
 vat = LpVariable.dicts('item', names_det, cat='Binary')
+
+model += lpSum(costs[i] * vat[i] for i in names_det)
+
+model += lpSum(costs[i] * vat[i] for i in names_det)
+model += lpSum(cn[i] * vat[i] for i in names_det) <= t_cn
+model += lpSum(dart[i] * vat[i] for i in names_det) <= t_dart
+model += lpSum(tour[i] * vat[i] for i in names_det) <= t_tour
+model += lpSum(cn_4[i] * vat[i] for i in names_det) <= t_cn4axe
+for i in names_det:
+    model += ((cn[i] + dart[i] + tour[i] + cn_4[i]) * vat[i]) <= c * b
+model += lpSum(cn[i] * vat[i] for i in names_det) <= c * b * count_cn
+model += lpSum(dart[i] * vat[i] for i in names_det) <= c * b * count_dart
+model += lpSum(tour[i] * vat[i] for i in names_det) <= c * b * count_tour
+model += lpSum(cn_4[i] * vat[i] for i in names_det) <= c * b * count_cn4axe
+model.solve()
+var_list = []
+print("Status:", LpStatus[model.status])
 
 costs_1 = []
 for n in vat:
@@ -40,31 +73,6 @@ for k in vat:
     cost_2.append(vat[k])
 cost_df = cost_df.assign(re_name=cost_2)
 
-model += lpSum(costs[i] * vat[i] for i in names_det)
-t_cn = 1600
-t_dart = 765
-t_tour = 1080
-'''
-количество центров 18шт
-количество дартов 16шт
-количество токарных 9 шт
-'''
-count_cn = 16
-count_dart = 14
-count_tour = 8
-model += lpSum(costs[i] * vat[i] for i in names_det)
-model += lpSum(cn[i] * vat[i] for i in names_det) <= t_cn
-model += lpSum(dart[i] * vat[i] for i in names_det) <= t_dart
-model += lpSum(tour[i] * vat[i] for i in names_det) <= t_tour
-for i in names_det:
-    model += ((cn[i] + dart[i] + tour[i]) * vat[i]) <= c * b
-model += lpSum(cn[i] * vat[i] for i in names_det) <= c * b * count_cn
-model += lpSum(dart[i] * vat[i] for i in names_det) <= c * b * count_dart
-model += lpSum(tour[i] * vat[i] for i in names_det) <= c * b * count_tour
-model.solve()
-var_list = []
-print("Status:", LpStatus[model.status])
-
 for v in model.variables():
     var_list_2 = [v.name, v.varValue]
     var_list.append(var_list_2)
@@ -72,11 +80,13 @@ for v in model.variables():
 df_var = pd.DataFrame(data=var_list, columns=['re_name', 'var'])
 # print(df_var.head())
 # print(cost_df.head())
-#df_out = df_var.merge(cost_df, on='re_name', how='outer')
-df_out = pd.concat([df_var, cost_df], axis=1)
+df_out = df_var.merge(cost_df, on='re_name', how='outer')
+# df_out = pd.concat([df_var, cost_df], axis=1)
 
 save_excel('result.xlsx', df_out)
 
-'''https://proglib.io/p/lineynoe-programmirovanie-praktika-resheniya-zadach-optimizacii-na-python-2020-11-26 
+'''
+https://proglib.io/p/lineynoe-programmirovanie-praktika-resheniya-zadach-optimizacii-na-python-2020-11-26 
 https://www.machinelearningmastery.ru/linear-programming-and-discrete-optimization-with-python-using-pulp
--449f3c5f6e99/ '''
+-449f3c5f6e99/ 
+'''
--- отработка.py
+++ отработка.py
@@ -2,3 +2,4 @@ import pandas as pd
 
 df_rest = pd.read_excel('restrictions.xlsx')
 df_rest.info()
+print(df_rest.loc[0, 'delta_smen_hours'])
